5a0a06962693adf8270de6feeff36d3a0e127a47
Colesiko <kotokopter2014@yandex.ru>
2023-07-23 22:23:37 +0500

    Добавление меню

--- My_Game1/Assets/Scripts/menu.cs
+++ My_Game1/Assets/Scripts/menu.cs
@@ -6,7 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
-    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
     public void ExitGame()
     {
         Application.Quit();
